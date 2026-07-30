f7f20951c03eb6d0e341c68e3f8ca9d233ebe5a4
nuriddinmamadaliyev002-cyber <nuriddinmamadaliyev002@gmail.com>
2026-07-30 23:48:45 +0500

    sana formati to'g'rilandi

--- innovateit-blog-frontend/js/blog.js
+++ innovateit-blog-frontend/js/blog.js
@@ -43,7 +43,7 @@ function fmtDate(iso) {
   const dd   = String(d.getDate()).padStart(2, '0');
   const mm   = String(d.getMonth() + 1).padStart(2, '0');
   const yyyy = d.getFullYear();
-  return `${dd}/${mm}/${yyyy}`;
+  return `${dd}.${mm}.${yyyy}`;
 }
 
 // Ko'rishlar sonini ko'z ikonkasi bilan chiqaruvchi HTML bo'lagi
